b3d574b13146e34883dda976ea7112bc09454246
Minh Thien <ldmthien3820@gmail.com>
2021-10-01 10:11:50 +0700

    Project v1 FE

--- src/App.js
+++ src/App.js
@@ -1,41 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useEffect, useState} from 'react';
-import usersApi from './api/usersApi';
-function App() {
-  const [usersList, setUsersList] = useState([]);
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import AdminTemplate from "./containers/AdminTemplate";
+import HomeTemplate from "./containers/HomeTemplate";
+import LoginPage from "./containers/HomeTemplate/LoginPage";
+import PageNotFound from "./containers/PageNotFound";
+import { routesAdmin, routesHome } from "./routes";
+import { connect } from "react-redux";
+import { CHANGE_THEME_REQUEST } from "./components/NavbarHome/modules/constant";
+import { useEffect } from "react";
+import CheckoutPage from "./containers/HomeTemplate/CheckoutPage";
+import AuthPage from "./containers/AdminTemplate/AuthPage";
+
+function App(props) {
+  const { themeStatus, handleChangeTheme } = props;
+
   useEffect(() => {
-  const fetchUsersList = async () => {
-  try {
-  // const params = { _page: 1, _limit: 10 };
-  const response = await usersApi.getAll();
-  console.log(response);
-  // setUsersList(response);
-  } catch (error) {
-  console.log('Failed to fetch product list: ', error);
-  }
-  }
-  fetchUsersList();
-  }, []);
-  // return <UserList userList={userList} />;
+    if (JSON.parse(localStorage.getItem("themeStatus"))) {
+      handleChangeTheme(JSON.parse(localStorage.getItem("themeStatus")));
+    } else {
+      localStorage.setItem("themeStatus", JSON.stringify(false));
+    }
+  });
+
+  const showLayout = (routes, text) => {
+    if (routes && routes.length > 0) {
+      return routes.map((item, index) => {
+        return text === "home" ? (
+          <HomeTemplate
+            key={index}
+            exact={item.exact}
+            path={item.path}
+            theme={themeStatus}
+            Component={item.component}
+          />
+        ) : (
+          <AdminTemplate
+            key={index}
+            exact={item.exact}
+            path={item.path}
+            Component={item.component}
+          />
+        );
+      });
+    }
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        {showLayout(routesHome, "home")}
+        {showLayout(routesAdmin, "admin")}
+
+        <Route exact={false} path="/auth" component={AuthPage} />
+        <Route exact={false} path="/dang-nhap" component={LoginPage} />
+        <Route exact={false} path="/checkout/:id" component={CheckoutPage} />
+        <Route path="*" component={PageNotFound} />
+      </Switch>
+    </BrowserRouter>
   );
-  }
-export default App;
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeStatus: state.changeThemeReducer.themeStatus,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  const action = (status) => ({
+    type: CHANGE_THEME_REQUEST,
+    payload: status,
+  });
+
+  return {
+    handleChangeTheme: (status) => {
+      dispatch(action(status));
+      localStorage.setItem("themeStatus", JSON.stringify(status));
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
